7659ec5524c18467ae5ce0db7f21554145007940
NZ <n.prokopyk@gmail.com>
2024-06-17 10:34:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Space News</h1>
+      </div>
+      <div className="newsContainer"></div>
+    </div>
+  );
+}
+
+export default App;
