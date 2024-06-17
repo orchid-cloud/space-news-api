339fca5a9b84e7ebe10a1f20264a24449c7719c4
NZ <n.prokopyk@gmail.com>
2024-06-17 11:15:37 +0200

    fetching data from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.spaceflightnewsapi.net/v4/articles/')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
   return (
     <div className="App">
       <div className="title">
