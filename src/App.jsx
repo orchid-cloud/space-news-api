f2d6719a056e1b3426f26c170e7dd97e1aef4a44
NZ <n.prokopyk@gmail.com>
2024-06-18 12:22:40 +0200

    displaying the api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [newsList, setNewsList] = useState([]);
+
   useEffect(() => {
     fetch('https://api.spaceflightnewsapi.net/v4/articles/')
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setNewsList([data]);
       });
-  });
+  }, []);
+  console.log({ newsList });
   return (
     <div className="App">
       <div className="title">
         <h1>Space News</h1>
       </div>
-      <div className="newsContainer"></div>
+      <div className="newsContainer">
+        {newsList.map((val, key) => (
+          <div key={key}>{val.results[0].title}</div>
+        ))}
+      </div>
     </div>
   );
 }
